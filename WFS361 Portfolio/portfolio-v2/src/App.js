3326b16728ddda60ca050ebc41cff296350c3c10
JohnyBoy <jeanvaneck031011@gmail.com>
2024-05-14 13:53:42 +0200

    The App.js code, still need to fix it. The Router Dom code is not working.

--- WFS361 Portfolio/portfolio-v2/src/App.js
+++ WFS361 Portfolio/portfolio-v2/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Components/Home Page';
+import AboutMe from './Components/About Me';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/about' component={AboutMe}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
